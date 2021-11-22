4041eddaece1721adc7a2dfe647b23a476227d7c
linzhaoqian <1203783169@qq.com>
2021-11-22 13:47:51 +0800

    11.22

--- Assets/Scripts/LevelThree/LockCoreController.cs
+++ Assets/Scripts/LevelThree/LockCoreController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class LockCoreController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject PosPoint;// Start is called before the first frame update
+    public GameObject PickLockController;
+    public float currentTime;
+    public float Mag;
+    public GameObject UpperHalf;
+    public GameObject LatterHalf;
+    private float invokeTime;
+   
+    void Awake()
     {
-        
+        PosPoint.transform.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Mag = (PosPoint.transform.position - UpperHalf.transform.position).magnitude;
+        if (Mag < 0.33)
+        {
+            UpperHalf.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, Mag * 3, 1); ;
+            LatterHalf.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, Mag * 3, 1); ;
+        }
+        if (Mag<0.05)
+        {
+            invokeTime += Time.deltaTime;
+            if (invokeTime > currentTime)
+            {
+                PickLockController.GetComponent<PickLockController>().UnlockCount++;
+                invokeTime = 0;
+            }
+        }
         
     }
 }
--- Assets/Scripts/LevelThree/PickLockController.cs
+++ Assets/Scripts/LevelThree/PickLockController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PickLockController : MonoBehaviour
 {
+    public int UnlockCount;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        UnlockCount = 0;
     }
-
     // Update is called once per frame
     void Update()
     {
